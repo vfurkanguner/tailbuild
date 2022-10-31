8988f4cb03dd1e1ed79905453b99e63a2566ef7a
vfurkanguner <vfurkanguner@gmail.com>
2022-10-31 23:54:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# taildrop
+ 
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-black">
+      selam
+    </div>
+  )
+}
+
+export default App
