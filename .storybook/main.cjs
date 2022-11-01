5b0df8d4bbc3f9074ec5f3ad4206df3b1b0ef952
vfurkanguner <vfurkanguner@gmail.com>
2022-11-01 21:13:40 +0300

    chores: add storybook

---
+++ .storybook/main.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+    "@storybook/addon-essentials",
+  ],
+  framework: "@storybook/react",
+};
