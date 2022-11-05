4522eaa278f5ee1e752e19bd6c1ce5f24be01d61
vfurkanguner <vfurkanguner@gmail.com>
2022-11-05 03:07:23 +0300

    style(empty screen)

--- src/components/EmptyScreen.tsx
+++ src/components/EmptyScreen.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import EmptyImg from "../assets/astro.png";
 
-type Props = {};
+type Props = {
+};
 
-export default function EmptyScreen({}: Props) {
+export default function EmptyScreen({ }: Props) {
   return (
-    <div className="w-full from-slate-900 overflow-hidden to-indigo-900 bg-gradient-to-t  text-white py-16 px-16 flex flex-col text-center gap-4 items-center justify-center">
-      <h1 className="text-3xl font-semibold leading-snug">
-        This is your drop zone
-        <br />
-        Start dragging components from the left sidebar
-      </h1>
-      <p className=" text-slate-100 font-light">
-        Start by adding components to your page from the left sidebar.
-      </p>
-      <img
-        src={EmptyImg}
-        alt="Image by catalyststuff"
-        className="w-full h-96 rounded-lg object-contain hover:animate-bounce"
-      />
+    <div>
+      <div className="absolute  w-full h-full from-slate-900 overflow-hidden to-indigo-900 bg-gradient-to-t  text-white py-16 px-16 flex flex-col text-center gap-4 items-center justify-center">
+        <h1 className="text-3xl font-semibold leading-snug">
+          This is your drop zone
+          <br />
+          Start dragging components from the left sidebar
+        </h1>
+        <p className=" text-slate-100 font-light">
+          Start by adding components to your page from the left sidebar.
+        </p>
+        <img
+          src={EmptyImg}
+          alt="Image by catalyststuff"
+          className="w-full h-96 rounded-lg object-contain hover:animate-bounce"
+        />
+      </div>
     </div>
   );
 }
